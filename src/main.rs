798bdae1cab21ca27f6c5cab970dc7aea07ea806
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-06-22 23:29:11 +0200

    Keep frame content on resize

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,14 @@ fn main() -> Result<(), Error> {
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
 
-    let canvas_size = LogicalSize::new(640, 480);
+    let mut canvas_size = LogicalSize::new(640, 480);
 
     let window = {
-        let default_size = LogicalSize::new(640., 480.);
-        let min_size = LogicalSize::new(128., 128.);
+        let min_size = LogicalSize::new(128, 128);
 
         WindowBuilder::new()
             .with_title("Lavagna")
-            .with_inner_size(default_size)
+            .with_inner_size(canvas_size)
             .with_min_inner_size(min_size)
             .build(&event_loop)
             .unwrap()
@@ -81,11 +80,26 @@ fn main() -> Result<(), Error> {
                 app.set_pressed(false);
             }
 
-            if let Some(size) = input.window_resized() {
-                debug!("Resize pixels to {}x{}", size.width, size.height);
-                pixels.resize_surface(size.width, size.height);
-                pixels.resize_buffer(size.width, size.height);
-                app.resize(size.width as isize, size.height as isize);
+            if let Some(new_size) = input.window_resized() {
+                debug!("Resize pixels to {}x{}", new_size.width, new_size.height);
+                let mut backup = Vec::from(pixels.get_frame());
+
+                pixels.get_frame().fill(0x00);
+                pixels.resize_surface(new_size.width, new_size.height);
+                pixels.resize_buffer(new_size.width, new_size.height);
+
+                copy_frame(
+                    pixels.get_frame(),
+                    new_size.width as usize,
+                    new_size.height as usize,
+                    backup.as_mut_slice(),
+                    canvas_size.width as usize,
+                    canvas_size.height as usize,
+                );
+
+                canvas_size.width = new_size.width;
+                canvas_size.height = new_size.height;
+                app.resize(new_size.width as isize, new_size.height as isize);
             }
 
             app.update(pixels.get_frame());
@@ -93,4 +107,19 @@ fn main() -> Result<(), Error> {
             window.request_redraw();
         }
     });
+}
+
+fn copy_frame(dst: &mut [u8], dst_w: usize, dst_h: usize, src: &mut [u8], src_w: usize, src_h: usize) {
+    let min_h = usize::min(dst_h, src_h);
+    let min_w = usize::min(dst_w, src_w);
+
+    for y in 0..min_h {
+        let dst_begin = dst_w * y;
+        let dst_end = dst_begin + min_w;
+        let src_begin = src_w * y;
+        let src_end = src_begin + min_w;
+        let dst_range = (4 * dst_begin)..(4 * dst_end);
+        let src_range = (4 * src_begin)..(4 * src_end);
+        dst[dst_range].copy_from_slice(&src[src_range]);
+    }
 }
