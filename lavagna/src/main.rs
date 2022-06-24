44a18afb44b04c70d556ad52409f9c3690ed12de
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-06-24 23:52:20 +0200

    Sync android and desktop versions

--- lavagna/src/main.rs
+++ lavagna/src/main.rs
@@ -1,18 +1,15 @@
 #![deny(clippy::all)]
 #![forbid(unsafe_code)]
 
-use lavagna_core::doc::{MutSketch};
+use lavagna_core::doc::MutSketch;
 use lavagna_core::App;
+use lavagna_core::doc::OwnedSketch;
 use log::error;
 use pixels::{Error, Pixels, SurfaceTexture};
 use winit::dpi::PhysicalSize;
-use winit::window::{CursorIcon};
-use winit::{
-    event::{Event, VirtualKeyCode, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-use winit::event::{ElementState, KeyboardInput, MouseButton};
+use winit::event::{TouchPhase, WindowEvent, Event, VirtualKeyCode, ElementState, KeyboardInput, MouseButton};
+use winit::window::{CursorIcon, Window, WindowBuilder};
+use winit::event_loop::{ControlFlow, EventLoop};
 
 fn run() -> Result<(), Error> {
     let event_loop = EventLoop::new();
@@ -30,13 +27,26 @@ fn run() -> Result<(), Error> {
     window.set_cursor_icon(CursorIcon::Crosshair);
 
     let mut app = App::new();
-
-    let mut pixels = {
-        let surface_texture = SurfaceTexture::new(canvas_size.width, canvas_size.height, &window);
-        Pixels::new(canvas_size.width, canvas_size.height, surface_texture).ok()
-    };
+    let mut frozen_sketch: Option<OwnedSketch> = None;
+    let mut pixels = resume(&window, canvas_size, frozen_sketch.take());
 
     event_loop.run(move |event, _, control_flow| {
+        if let Event::Resumed = event {
+            canvas_size = window.inner_size();
+            pixels = resume(&window, canvas_size, frozen_sketch.take());
+
+            // Prevent drawing a line from the last location when resuming
+            app.set_pressed(false);
+        }
+
+        if let Event::Suspended = event {
+            if let Some(mut pixels) = pixels.take() {
+                let sketch =
+                    MutSketch::new(pixels.get_frame(), canvas_size.width, canvas_size.height);
+                frozen_sketch = Some(sketch.to_owned());
+            }
+        }
+
         if let Some(mut pixels) = pixels.as_mut() {
             match event {
                 Event::RedrawRequested(_) => {
@@ -80,6 +90,24 @@ fn run() -> Result<(), Error> {
                     }
                 }
                 Event::WindowEvent {
+                    event: WindowEvent::Touch(touch),
+                    ..
+                } => {
+                    match touch.phase {
+                        TouchPhase::Started => {
+                            app.set_pressed(true);
+                        }
+                        TouchPhase::Ended => {
+                            app.set_pressed(false);
+                        }
+                        _ => (),
+                    }
+
+                    if let Ok((x, y)) = pixels.window_pos_to_pixel(touch.location.into()) {
+                        app.set_cursor_position(x as isize, y as isize);
+                    }
+                }
+                Event::WindowEvent {
                     event: WindowEvent::MouseInput { state, button, .. },
                     ..
                 } => {
@@ -108,6 +136,25 @@ fn run() -> Result<(), Error> {
     });
 }
 
+fn resume(
+    window: &Window,
+    new_size: PhysicalSize<u32>,
+    frozen_sketch: Option<OwnedSketch>,
+) -> Option<Pixels> {
+    let surface_texture = SurfaceTexture::new(new_size.width, new_size.height, &window);
+    let mut pixels = Pixels::new(new_size.width, new_size.height, surface_texture).ok()?;
+
+    pixels.get_frame().fill(0x00);
+
+    let mut new_sketch = MutSketch::new(pixels.get_frame(), new_size.width, new_size.height);
+
+    if let Some(old_sketch) = &frozen_sketch {
+        new_sketch.copy_from(&old_sketch.as_sketch());
+    }
+
+    Some(pixels)
+}
+
 fn resize_buffer(pixels: &mut Pixels, canvas_size: PhysicalSize<u32>, new_size: PhysicalSize<u32>) {
     let old_sketch =
         MutSketch::new(pixels.get_frame(), canvas_size.width, canvas_size.height).to_owned();
@@ -120,6 +167,7 @@ fn resize_buffer(pixels: &mut Pixels, canvas_size: PhysicalSize<u32>, new_size: 
 
     new_sketch.copy_from(&old_sketch.as_sketch());
 }
+
 
 fn main() -> Result<(), Error> {
     env_logger::init();
--- lavagna_android/src/lib.rs
+++ lavagna_android/src/lib.rs
@@ -7,15 +7,11 @@ use lavagna_core::doc::OwnedSketch;
 use log::error;
 use pixels::{Error, Pixels, SurfaceTexture};
 use winit::dpi::PhysicalSize;
-use winit::event::{TouchPhase, WindowEvent};
-use winit::window::Window;
-use winit::{
-    event::Event,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use winit::event::{TouchPhase, WindowEvent, Event, VirtualKeyCode, ElementState, KeyboardInput, MouseButton};
+use winit::window::{CursorIcon, Window, WindowBuilder};
+use winit::event_loop::{ControlFlow, EventLoop};
 
-pub fn run() -> Result<(), Error> {
+fn run() -> Result<(), Error> {
     let event_loop = EventLoop::new();
     let mut canvas_size = PhysicalSize::new(640, 480);
 
@@ -28,9 +24,11 @@ pub fn run() -> Result<(), Error> {
             .unwrap()
     };
 
-    let mut pixels: Option<Pixels> = None;
+    window.set_cursor_icon(CursorIcon::Crosshair);
+
     let mut app = App::new();
     let mut frozen_sketch: Option<OwnedSketch> = None;
+    let mut pixels = None;
 
     event_loop.run(move |event, _, control_flow| {
         if let Event::Resumed = event {
@@ -49,7 +47,7 @@ pub fn run() -> Result<(), Error> {
             }
         }
 
-        if let Some(pixels) = pixels.as_mut() {
+        if let Some(mut pixels) = pixels.as_mut() {
             match event {
                 Event::RedrawRequested(_) => {
                     let sketch =
@@ -69,12 +67,28 @@ pub fn run() -> Result<(), Error> {
                     window.request_redraw();
                 }
                 Event::WindowEvent {
+                    event: WindowEvent::Resized(new_size),
+                    ..
+                } => {
+                    if canvas_size != new_size {
+                        resize_buffer(&mut pixels, canvas_size, new_size);
+                        canvas_size = new_size;
+                    }
+                }
+                Event::WindowEvent {
                     event: WindowEvent::CloseRequested,
                     ..
                 } => {
                     *control_flow = ControlFlow::Exit;
                 }
-
+                Event::WindowEvent {
+                    event: WindowEvent::CursorMoved { position, .. },
+                    ..
+                } => {
+                    if let Ok((x, y)) = pixels.window_pos_to_pixel(position.into()) {
+                        app.set_cursor_position(x as isize, y as isize);
+                    }
+                }
                 Event::WindowEvent {
                     event: WindowEvent::Touch(touch),
                     ..
@@ -94,12 +108,28 @@ pub fn run() -> Result<(), Error> {
                     }
                 }
                 Event::WindowEvent {
+                    event: WindowEvent::MouseInput { state, button, .. },
+                    ..
+                } => {
+                    match (button, state) {
+                        (MouseButton::Left, ElementState::Pressed) => app.set_pressed(true),
+                        (MouseButton::Left, ElementState::Released) => app.set_pressed(false),
+                        _ => (),
+                    }
+                }
+                Event::WindowEvent {
                     event: WindowEvent::KeyboardInput { input, .. },
                     ..
                 } => {
-                    log::info!("input: {:?}", input);
+                    match input {
+                        KeyboardInput { state: ElementState::Released, virtual_keycode: Some(VirtualKeyCode::Escape), .. } => { *control_flow = ControlFlow::Exit; }
+                        KeyboardInput { state: ElementState::Released, virtual_keycode: Some(VirtualKeyCode::X), .. } => { app.clear_all(); }
+                        KeyboardInput { state: ElementState::Released, virtual_keycode: Some(VirtualKeyCode::C), .. } => { app.change_color(); }
+                        KeyboardInput { state: ElementState::Released, virtual_keycode: Some(VirtualKeyCode::Z), .. } => { app.resume(); }
+                        KeyboardInput { state: ElementState::Released, virtual_keycode: Some(VirtualKeyCode::S), .. } => { app.take_snapshot(); }
+                        _ => (),
+                    }
                 }
-
                 _ => (),
             }
         }
@@ -125,17 +155,30 @@ fn resume(
     Some(pixels)
 }
 
+fn resize_buffer(pixels: &mut Pixels, canvas_size: PhysicalSize<u32>, new_size: PhysicalSize<u32>) {
+    let old_sketch =
+        MutSketch::new(pixels.get_frame(), canvas_size.width, canvas_size.height).to_owned();
+
+    pixels.get_frame().fill(0x00);
+    pixels.resize_surface(new_size.width, new_size.height);
+    pixels.resize_buffer(new_size.width, new_size.height);
+
+    let mut new_sketch = MutSketch::new(pixels.get_frame(), new_size.width, new_size.height);
+
+    new_sketch.copy_from(&old_sketch.as_sketch());
+}
+
 #[cfg(target_os = "android")]
 #[cfg_attr(
-    target_os = "android",
-    ndk_glue::main(
-        backtrace = "on",
-        logger(
-            level = "info",
-            filter = "debug,wgpu_hal::vulkan=error",
-            tag = "lavagna"
-        )
-    )
+target_os = "android",
+ndk_glue::main(
+backtrace = "on",
+logger(
+level = "info",
+filter = "debug,wgpu_hal::vulkan=error",
+tag = "lavagna"
+)
+)
 )]
 fn main() {
     run().unwrap();
