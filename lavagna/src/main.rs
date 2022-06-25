72b967d1e432a40d9f3c69eee3ceef33f037c327
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-06-25 16:10:21 +0200

    Add cli parameter for collaboration url

--- lavagna/src/main.rs
+++ lavagna/src/main.rs
@@ -1,13 +1,24 @@
 #![deny(clippy::all)]
 #![forbid(unsafe_code)]
 
+use clap::Parser;
 use lavagna_pixels::{run, Error, Opt};
+
+/// The uncluttered blackboard
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    collab_url: Option<String>,
+}
 
 fn main() -> Result<(), Error> {
     env_logger::init();
 
+    let args = Args::parse();
+
     let opt = Opt {
-        collab_url: Some("ws://localhost:3536/example_room".to_string()),
+        collab_url: args.collab_url,
     };
 
     run(opt)
