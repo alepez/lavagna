9abb739fc345e0c5a29a6fae582a89b93f259912
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-10-27 00:49:38 +0200

    WIP android getIntent from NativeActivity

--- lavagna_android/src/lib.rs
+++ lavagna_android/src/lib.rs
@@ -1,10 +1,9 @@
 #![deny(clippy::all)]
-#![forbid(unsafe_code)]
 
-#[cfg(target_os = "android")]
+// #[cfg(target_os = "android")]
 use lavagna_pixels::{run, Opt};
 
-#[cfg(target_os = "android")]
+// #[cfg(target_os = "android")]
 #[cfg_attr(
     target_os = "android",
     ndk_glue::main(
@@ -16,9 +15,33 @@ use lavagna_pixels::{run, Opt};
         )
     )
 )]
+#[allow(dead_code)]
 fn main() {
+    let uri = get_collab_uri_from_intent().ok();
+    log::info!("uri: {:?}", uri);
+
     // Collaboration is not yet supported on Android
     let opt = Opt { collab: None };
 
     run(opt).unwrap();
+}
+
+fn get_collab_uri_from_intent() -> Result<String, Box<dyn std::error::Error>> {
+    // lavagna+wss://lavagna-server.herokuapp.com/9db2ca48-6f12-4f57-8ae7-0c4bf83d0fb0
+
+    let ctx = ndk_context::android_context();
+    let vm = unsafe { jni::JavaVM::from_raw(ctx.vm().cast()) }?;
+    let env = vm.attach_current_thread()?;
+    let class_ctx = env.find_class("android/content/Context")?;
+
+    let intent = env
+        .call_method(
+            ctx.context().cast(),
+            "getIntent",
+            "()Landroid/content/Intent;",
+            &[],
+        )?
+        .l()?;
+
+    Ok("smile!".to_string())
 }
