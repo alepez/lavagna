c5f2b78eba458064b7306951fdc0b30d9c58ad23
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-15 23:54:00 +0200

    Add Pen component and update it by mouse position

--- crates/lavagna/src/main.rs
+++ crates/lavagna/src/main.rs
@@ -1,4 +1,7 @@
-use bevy::{input::mouse::*, prelude::*};
+use bevy::{
+    input::{mouse::*, ButtonState},
+    prelude::*,
+};
 
 fn main() {
     App::new()
@@ -16,17 +19,58 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(Pen::new_local());
 }
 
 fn mouse_system(
     mut mouse_button_input_events: EventReader<MouseButtonInput>,
     mut cursor_moved_events: EventReader<CursorMoved>,
+    mut query: Query<&mut Pen>,
 ) {
-    for event in mouse_button_input_events.iter() {
-        info!("{:?}", event);
-    }
+    for mut pen in &mut query {
+        if pen.local {
+            for event in mouse_button_input_events.iter() {
+                match event {
+                    MouseButtonInput {
+                        button: MouseButton::Left,
+                        state: ButtonState::Pressed,
+                    } => {
+                        pen.pressed = true;
+                    }
+                    MouseButtonInput {
+                        button: MouseButton::Left,
+                        state: ButtonState::Released,
+                    } => {
+                        pen.pressed = false;
+                    }
+                    _ => {}
+                }
+            }
 
-    for event in cursor_moved_events.iter() {
-        info!("{:?}", event);
+            for event in cursor_moved_events.iter() {
+                pen.x = event.position[0] as i64;
+                pen.y = event.position[1] as i64;
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+struct Pen {
+    pressed: bool,
+    local: bool,
+    x: i64,
+    y: i64,
+}
+
+impl Pen {
+    fn new_local() -> Self {
+        Self {
+            pressed: true,
+            local: true,
+            x: 0,
+            y: 0,
+        }
     }
 }
