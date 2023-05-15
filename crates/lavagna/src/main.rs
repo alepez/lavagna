de5ddb274ea98daa66ed36628a88eb3f5f940704
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-15 23:30:23 +0200

    Setup window and camera

--- crates/lavagna/src/main.rs
+++ crates/lavagna/src/main.rs
@@ -1,3 +1,18 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (640., 480.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
