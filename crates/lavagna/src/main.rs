3a400207582c6453c6b9218222717b8f7521fa8e
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-16 00:15:19 +0200

    Add debug text

--- crates/lavagna/src/main.rs
+++ crates/lavagna/src/main.rs
@@ -1,4 +1,5 @@
 use bevy::{
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
     input::{mouse::*, ButtonState},
     prelude::*,
 };
@@ -14,13 +15,35 @@ fn main() {
         }))
         .add_startup_system(setup)
         .add_system(mouse_system)
+        .add_system(debug_test_system)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(Pen::new_local());
+
+    commands.spawn((
+        TextBundle::from_section(
+            "--",
+            TextStyle {
+                font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                font_size: 50.0,
+                color: Color::WHITE,
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                top: Val::Px(5.0),
+                left: Val::Px(15.0),
+                ..default()
+            },
+            ..default()
+        }),
+        DebugText,
+    ));
 }
 
 fn mouse_system(
@@ -64,6 +87,9 @@ struct Pen {
     y: i64,
 }
 
+#[derive(Component)]
+struct DebugText;
+
 impl Pen {
     fn new_local() -> Self {
         Self {
@@ -73,4 +99,33 @@ impl Pen {
             y: 0,
         }
     }
+}
+
+fn debug_test_system(
+    time: Res<Time>,
+    diagnostics: Res<Diagnostics>,
+    mut text: Query<&mut Text, With<DebugText>>,
+    mut pen: Query<&mut Pen>,
+) {
+    let mut text = text.single_mut();
+    let pen = pen.single_mut();
+
+    let x = pen.x;
+    let y = pen.y;
+
+    let mut fps = 0.0;
+    if let Some(fps_diagnostic) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+        if let Some(fps_smoothed) = fps_diagnostic.smoothed() {
+            fps = fps_smoothed;
+        }
+    }
+
+    let mut frame_time = time.delta_seconds_f64();
+    if let Some(frame_time_diagnostic) = diagnostics.get(FrameTimeDiagnosticsPlugin::FRAME_TIME) {
+        if let Some(frame_time_smoothed) = frame_time_diagnostic.smoothed() {
+            frame_time = frame_time_smoothed;
+        }
+    }
+
+    text.sections[0].value = format!("{fps:.1} fps\n{frame_time:.3} ms/frame\n{x}:{y}\n",);
 }
