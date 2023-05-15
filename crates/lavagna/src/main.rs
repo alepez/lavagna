296f5f576a79e9c146c9ecf237be1f6e5094de1c
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-16 00:40:01 +0200

    Auto convert coordinate system

--- crates/lavagna/src/main.rs
+++ crates/lavagna/src/main.rs
@@ -2,6 +2,7 @@ use bevy::{
     diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
     input::{mouse::*, ButtonState},
     prelude::*,
+    window::Window,
 };
 
 fn main() {
@@ -15,12 +16,16 @@ fn main() {
         }))
         .add_startup_system(setup)
         .add_system(mouse_system)
+        .add_system(cursor_system)
         .add_system(debug_test_system)
         .run();
 }
 
+#[derive(Component)]
+struct MainCamera;
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((Camera2dBundle::default(), MainCamera));
 
     commands.spawn((Pen::new(), LocalPen));
 
@@ -48,10 +53,10 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 fn mouse_system(
     mut mouse_button_input_events: EventReader<MouseButtonInput>,
-    mut cursor_moved_events: EventReader<CursorMoved>,
-    mut pen: Query<&mut Pen, With<LocalPen>>,
+    mut pen_q: Query<&mut Pen, With<LocalPen>>,
 ) {
-    let mut pen = pen.single_mut();
+    let mut pen = pen_q.single_mut();
+
     for event in mouse_button_input_events.iter() {
         match event {
             MouseButtonInput {
@@ -69,10 +74,24 @@ fn mouse_system(
             _ => {}
         }
     }
+}
 
-    for event in cursor_moved_events.iter() {
-        pen.x = event.position[0] as i64;
-        pen.y = event.position[1] as i64;
+fn cursor_system(
+    window_q: Query<&Window>,
+    camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    mut pen_q: Query<&mut Pen, With<LocalPen>>,
+) {
+    let (camera, camera_transform) = camera_q.single();
+    let mut pen = pen_q.single_mut();
+    let window = window_q.single();
+
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        pen.x = world_position[0] as i64;
+        pen.y = world_position[1] as i64;
     }
 }
 
@@ -123,7 +142,7 @@ fn debug_test_system(
     let x = pen.x;
     let y = pen.y;
     let pressed = pen.pressed;
-    let pen = format!("{x}:{y} {pressed}");
+    let pen = format!("{x:+05}:{y:+05} {pressed}");
 
     text.sections[0].value = format!("{fps}\n{frame_time}\n{pen}\n",);
 }
