f16ebdcf6c332a8e5997cbf8f219f1e7bfa6b99c
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-15 23:35:11 +0200

    Add mouse motion event

--- crates/lavagna/src/main.rs
+++ crates/lavagna/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{input::mouse::*, prelude::*};
 
 fn main() {
     App::new()
@@ -10,9 +10,23 @@ fn main() {
             ..default()
         }))
         .add_startup_system(setup)
+        .add_system(mouse_system)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn mouse_system(
+    mut mouse_button_input_events: EventReader<MouseButtonInput>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
+) {
+    for event in mouse_button_input_events.iter() {
+        info!("{:?}", event);
+    }
+
+    for event in cursor_moved_events.iter() {
+        info!("{:?}", event);
+    }
 }
