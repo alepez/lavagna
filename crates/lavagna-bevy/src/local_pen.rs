6a47a6e824775f446666730430f7dc5d2fb32bd4
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-16 09:54:04 +0200

    Add visible pen

--- crates/lavagna-bevy/src/local_pen.rs
+++ crates/lavagna-bevy/src/local_pen.rs
@@ -14,15 +14,26 @@ struct LocalPen;
 
 impl Plugin for LocalPenPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(startup).add_system(update);
+        app.add_startup_system(startup)
+            .add_system(handle_user_input)
+            .add_system(update_sprite_position);
     }
 }
 
-fn startup(mut commands: Commands) {
-    commands.spawn((Pen::new(), LocalPen));
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((LocalPen, Pen::new()));
+
+    commands.spawn((
+        LocalPen,
+        SpriteBundle {
+            texture: asset_server.load("sprites/pen.png"),
+            visibility: Visibility::Hidden,
+            ..default()
+        },
+    ));
 }
 
-fn update(
+fn handle_user_input(
     window_q: Query<&Window>,
     camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
     mut mouse_button_input_events: EventReader<MouseButtonInput>,
@@ -58,4 +69,21 @@ fn update(
             _ => {}
         }
     }
+}
+
+fn update_sprite_position(
+    pen_q: Query<&mut Pen, With<LocalPen>>,
+    mut sprite_transform_q: Query<&mut Transform, With<LocalPen>>,
+    mut sprite_visibility_q: Query<&mut Visibility, With<LocalPen>>,
+) {
+    let pen = pen_q.single();
+    let mut t = sprite_transform_q.single_mut();
+    let mut v = sprite_visibility_q.single_mut();
+    t.translation = Vec3::new(pen.x as f32, pen.y as f32, 0.);
+
+    *v = if pen.pressed {
+        Visibility::Visible
+    } else {
+        Visibility::Hidden
+    };
 }
