539f4b0daec972ebe27f77c0f879f0067741f7f9
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-17 18:57:00 +0200

    Show current chalk size

--- crates/lavagna-bevy/src/local_chalk.rs
+++ crates/lavagna-bevy/src/local_chalk.rs
@@ -43,15 +43,14 @@ impl Plugin for LocalPenPlugin {
 }
 
 fn startup(mut commands: Commands, chalk_config: Res<LocalChalkConfig>) {
-    commands.spawn((LocalChalk, Chalk::new()));
-
     let shape = shapes::Circle {
-        radius: chalk_config.line_width as f32 / 2.,
+        radius: 1.0,
         center: Vec2::new(0.0, 0.0),
     };
 
     commands.spawn((
         LocalChalk,
+        Chalk::new(),
         ShapeBundle {
             path: GeometryBuilder::build_as(&shape),
             ..default()
@@ -138,13 +137,15 @@ fn is_updated(old_chalk: &Chalk, new_chalk: &Chalk) -> bool {
 
 fn update_config(
     chalk_config: Res<LocalChalkConfig>,
-    mut chalk_q: Query<&mut Chalk, With<LocalChalk>>,
-    mut fill_q: Query<&mut Fill, With<LocalChalk>>,
+    mut chalk_q: Query<(&mut Chalk, &mut Fill, &mut Transform), With<LocalChalk>>,
 ) {
-    let chalk = &mut chalk_q.single_mut();
+    let (mut chalk, mut fill, mut transform) = chalk_q.single_mut();
+
     chalk.color = chalk_config.color;
     chalk.line_width = chalk_config.line_width;
 
-    let fill: &mut Fill = &mut fill_q.single_mut();
     *fill = Fill::color(chalk_config.color);
+
+    let scale = chalk.line_width as f32 / 2.0;
+    transform.scale = Vec3::new(scale, scale, scale);
 }
